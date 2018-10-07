f049e69786c1c68bebac209f6faf65bb0ed98b3c
Wessel Mast <mastwessel@gmail.com>
2018-10-07 16:44:37 +0200

    meh
    Ik heb alles maar gwn gedelete ;P zo opnieuw proberen

--- OpenGL/src/generation/WorldGeneration.h
+++ OpenGL/src/generation/WorldGeneration.h
@@ -9,12 +9,11 @@ class WorldGeneration
 private: //section for private variables
 	int size, height, amtOfChunks, amtOfOctaves, num;
 	ChunkGenerator* chunkGen;
-	std::vector<Chunk*> chunks;
 	ChunkManager* man;
 	Camera* player;
 public: //section for public functions
 	WorldGeneration(ChunkManager* man, Camera* player);
-	std::vector<Chunk*> generateWorld(int size, int height, int amtOfChunks, int amtOfOctaves);
+	void generateWorld(int size, int height, int amtOfChunks, int amtOfOctaves);
 	void updateChunks();
 	std::vector<glm::vec2> getNeighbours();
 	~WorldGeneration();
