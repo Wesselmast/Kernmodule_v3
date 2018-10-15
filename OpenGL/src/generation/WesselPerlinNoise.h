5c156c476e40fbe47564b274c96d064625c903ba
Wessel Mast <mastwessel@gmail.com>
2018-10-16 00:00:42 +0200

    PN no more clipping + optimisations

--- OpenGL/src/generation/WesselPerlinNoise.h
+++ OpenGL/src/generation/WesselPerlinNoise.h
@@ -4,7 +4,7 @@
 
 class WesselPerlinNoise {
 private: //section for private variables
-	std::vector<int> perm;
+	int* perm;
 private: //section for private functions
 	float fade(float t);
 	float lerp(float t, float a, float b);
