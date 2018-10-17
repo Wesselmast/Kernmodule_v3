f1850d49b70cb258b09e7cc9f27da789d2062e41
Jelle <jellebooij@outlook.com>
2018-10-17 17:52:48 +0200

    asf

--- OpenGL/src/generation/Chunk.cpp
+++ OpenGL/src/generation/Chunk.cpp
@@ -5,7 +5,7 @@ Chunk::Chunk() : size(0), height(0), xPos(0), yPos(0), zPos(0)
 }
 
 Chunk::Chunk(int size, int height, int xPos, int yPos, int zPos) :
-	size(size),height(height), xPos(xPos), yPos(yPos), zPos(zPos), blocks(size, std::vector<std::vector<char>>(height, std::vector<char>(size,0)))
+	size(size),height(height), xPos(xPos), yPos(yPos), zPos(zPos), blocks(size, std::vector<std::vector<char>>(height, std::vector<char>(size,(char)blockType::Air)))
 {	
 }
 
