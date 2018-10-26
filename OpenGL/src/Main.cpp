b0d6391bf7f357377a2979d7e03d7054e64fa2de
Wessel Mast <mastwessel@gmail.com>
2018-10-26 19:02:48 +0200

    asd

--- OpenGL/src/Main.cpp
+++ OpenGL/src/Main.cpp
@@ -28,7 +28,7 @@
 #include "ChunkManager.h"
 #include "UiRenderer.h"
 
-const bool FULLSCREEN = true;
+const bool FULLSCREEN = false;
 
 float deltaTime = 0.0f;	// Time between current frame and last frame
 float lastFrame = 0.0f; // Time of last frame
